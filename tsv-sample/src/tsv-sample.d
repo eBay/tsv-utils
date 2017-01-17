5efe69d73dc8f40832507f83fd3d1a9adede622e
Jon Degenhardt <jdegenhardt@ebay.com>
2017-01-16 18:33:24 -0800

    tsv-sample: initial unit tests.

--- tsv-sample/src/tsv-sample.d
+++ tsv-sample/src/tsv-sample.d
@@ -286,3 +286,448 @@ unittest
     assertThrown(getFieldValue!double("123", 1, '\t', "unittest", 1));
     assertThrown(getFieldValue!double("123", 1, '\t', "unittest", 2));
 }
+
+/* Unit tests for the main program start here.
+ */
+version(unittest)
+{
+    /* Unit test helper functions. */
+
+    import unittest_utils;   // tsv unit test helpers, from common/src/.
+    import std.conv;
+
+    void testTsvSample(string[] cmdArgs, string[][] expected)
+    {
+        import std.array : appender;
+        import std.format : format;
+        
+        assert(cmdArgs.length > 0, "[testTsvSample] cmdArgs must not be empty.");
+
+        auto formatAssertMessage(T...)(string msg, T formatArgs)
+        {
+            auto formatString = "[testTsvSample] %s: " ~ msg;
+            return format(formatString, cmdArgs[0], formatArgs);
+        }
+
+        TsvSampleOptions cmdopt;
+        auto savedCmdArgs = cmdArgs.to!string;
+        auto r = cmdopt.processArgs(cmdArgs);
+        assert(r[0], formatAssertMessage("Invalid command lines arg: '%s'.", savedCmdArgs));
+
+        auto output = appender!(char[])();
+        weightedReservoirSamplingES(cmdopt, output);
+        auto expectedOutput = expected.tsvDataToString;
+
+        assert(output.data == expectedOutput,
+               formatAssertMessage(
+                   "Result != expected:\n=====Expected=====\n%s=====Actual=======\n%s==================",
+                   expectedOutput.to!string, output.data.to!string));
+    }
+ }
+
+unittest
+{
+    import std.path : buildPath;
+    import std.file : rmdirRecurse;
+    import std.format : format;
+
+    auto testDir = makeUnittestTempDir("tsv_sample");
+    scope(exit) testDir.rmdirRecurse;
+
+    /* Tabular data sets and expected results using the built-in static seed.
+     * Tests are run by writing the data set to a file and reading running
+     * the main routine. The expected results were verified by hand prior to
+     * inclusion in the test.
+     */
+
+    /* Empty file. */
+    string[][] dataEmpty = [];
+    string fpath_dataEmpty = buildPath(testDir, "dataEmpty.tsv");
+    writeUnittestTsvFile(fpath_dataEmpty, dataEmpty);
+
+    /* 3x1, header only. */
+    string[][] data3x0 = [["field_a", "field_b", "field_c"]];
+    string fpath_data3x0 = buildPath(testDir, "data3x0.tsv");
+    writeUnittestTsvFile(fpath_data3x0, data3x0);
+
+    /* 3x1 */
+    string[][] data3x1 =
+        [["field_a", "field_b", "field_c"],
+         ["tan", "タン", "8.5"]];
+
+    string fpath_data3x1 = buildPath(testDir, "data3x1.tsv");
+    string fpath_data3x1_noheader = buildPath(testDir, "data3x1_noheader.tsv");
+    writeUnittestTsvFile(fpath_data3x1, data3x1);
+    writeUnittestTsvFile(fpath_data3x1_noheader, data3x1[1..$]);
+
+    string[][] data3x2 =
+        [["field_a", "field_b", "field_c"],
+         ["brown", "褐色", "29.2"],
+         ["gray", "グレー", "6.2"]];
+
+    /* 3x2 */
+    string fpath_data3x2 = buildPath(testDir, "data3x2.tsv");
+    string fpath_data3x2_noheader = buildPath(testDir, "data3x2_noheader.tsv");
+    writeUnittestTsvFile(fpath_data3x2, data3x2);
+    writeUnittestTsvFile(fpath_data3x2_noheader, data3x2[1..$]);
+
+    string[][] data3x2ExpectedNoWt =
+        [["field_a", "field_b", "field_c"],
+         ["gray", "グレー", "6.2"],
+         ["brown", "褐色", "29.2"]];
+
+    /* 3x3 */
+    string[][] data3x3 =
+        [["field_a", "field_b", "field_c"],
+         ["orange", "オレンジ", "2.5"],
+         ["pink", "ピンク", "1.1"],
+         ["purple", "紫の", "42"]];
+
+    string fpath_data3x3 = buildPath(testDir, "data3x3.tsv");
+    string fpath_data3x3_noheader = buildPath(testDir, "data3x3_noheader.tsv");
+    writeUnittestTsvFile(fpath_data3x3, data3x3);
+    writeUnittestTsvFile(fpath_data3x3_noheader, data3x3[1..$]);
+
+    string[][] data3x3ExpectedNoWt =
+        [["field_a", "field_b", "field_c"],
+         ["purple", "紫の", "42"],
+         ["pink", "ピンク", "1.1"],
+         ["orange", "オレンジ", "2.5"]];
+
+    /* 3x6 */
+    string[][] data3x6 =
+        [["field_a", "field_b", "field_c"],
+         ["red", "赤", "23.8"],
+         ["green", "緑", "0.0072"],
+         ["white", "白", "1.65"],
+         ["yellow", "黄", "12"],
+         ["blue", "青", "12"],
+         ["black", "黒", "0.983"]];
+    string fpath_data3x6 = buildPath(testDir, "data3x6.tsv");
+    string fpath_data3x6_noheader = buildPath(testDir, "data3x6_noheader.tsv");
+    writeUnittestTsvFile(fpath_data3x6, data3x6);    
+    writeUnittestTsvFile(fpath_data3x6_noheader, data3x6[1..$]);    
+
+    string[][] data3x6ExpectedNoWt =
+        [["field_a", "field_b", "field_c"],
+         ["yellow", "黄", "12"],
+         ["black", "黒", "0.983"],
+         ["blue", "青", "12"],
+         ["white", "白", "1.65"],
+         ["green", "緑", "0.0072"],
+         ["red", "赤", "23.8"]];
+
+    string[][] data3x6ExpectedNoWtProbs =
+        [["random_weight", "field_a", "field_b", "field_c"],
+         ["0.960555462865159", "yellow", "黄", "12"],
+         ["0.757101539289579", "black", "黒", "0.983"],
+         ["0.525259808870032", "blue", "青", "12"],
+         ["0.492878549499437", "white", "白", "1.65"],
+         ["0.159293440869078", "green", "緑", "0.0072"],
+         ["0.010968807619065", "red", "赤", "23.8"]];
+
+    string[][] data3x6ExpectedWt3Probs =
+        [["random_weight", "field_a", "field_b", "field_c"],
+         ["0.996651987576454", "yellow", "黄", "12"],
+         ["0.947758848098367", "blue", "青", "12"],
+         ["0.827282346822867", "red", "赤", "23.8"],
+         ["0.75346697377182", "black", "黒", "0.983"],
+         ["0.651301034964225", "white", "白", "1.65"],
+         ["1.56369437128799e-111", "green", "緑", "0.0072"]];
+
+    string[][] data3x6ExpectedWt3 =
+        [["field_a", "field_b", "field_c"],
+         ["yellow", "黄", "12"],
+         ["blue", "青", "12"],
+         ["red", "赤", "23.8"],
+         ["black", "黒", "0.983"],
+         ["white", "白", "1.65"],
+         ["green", "緑", "0.0072"]];
+
+    /* Combo 1: 3x3, 3x1, 3x6, 3x2. No data files, only expected results. */
+    string[][] combo1ExpectedNoWt =
+        [["field_a", "field_b", "field_c"],
+         ["yellow", "黄", "12"],
+         ["tan", "タン", "8.5"],
+         ["brown", "褐色", "29.2"],
+         ["green", "緑", "0.0072"],
+         ["red", "赤", "23.8"],
+         ["purple", "紫の", "42"],
+         ["black", "黒", "0.983"],
+         ["white", "白", "1.65"],
+         ["gray", "グレー", "6.2"],
+         ["blue", "青", "12"],
+         ["pink", "ピンク", "1.1"],
+         ["orange", "オレンジ", "2.5"]];
+
+    string[][] combo1ExpectedNoWtProbs =
+        [["random_weight", "field_a", "field_b", "field_c"],
+         ["0.970885202754289", "yellow", "黄", "12"],
+         ["0.960555462865159", "tan", "タン", "8.5"],
+         ["0.817568943137303", "brown", "褐色", "29.2"],
+         ["0.757101539289579", "green", "緑", "0.0072"],
+         ["0.525259808870032", "red", "赤", "23.8"],
+         ["0.492878549499437", "purple", "紫の", "42"],
+         ["0.470815070671961", "black", "黒", "0.983"],
+         ["0.383881829213351", "white", "白", "1.65"],
+         ["0.292159906122833", "gray", "グレー", "6.2"],
+         ["0.240332160145044", "blue", "青", "12"],
+         ["0.159293440869078", "pink", "ピンク", "1.1"],
+         ["0.010968807619065", "orange", "オレンジ", "2.5"]];
+
+    string[][] combo1ExpectedWt3Probs =
+        [["random_weight", "field_a", "field_b", "field_c"],
+         ["0.997540775237188", "yellow", "黄", "12"],
+         ["0.995276654400888", "tan", "タン", "8.5"],
+         ["0.993125789457417", "brown", "褐色", "29.2"],
+         ["0.983296025533894", "purple", "紫の", "42"],
+         ["0.973309619380837", "red", "赤", "23.8"],
+         ["0.887975515217396", "blue", "青", "12"],
+         ["0.819992304890418", "gray", "グレー", "6.2"],
+         ["0.559755692042509", "white", "白", "1.65"],
+         ["0.464721356092057", "black", "黒", "0.983"],
+         ["0.188245827041913", "pink", "ピンク", "1.1"],
+         ["0.164461318532999", "orange", "オレンジ", "2.5"],
+         ["1.64380869310205e-17", "green", "緑", "0.0072"]];
+
+    string[][] combo1ExpectedWt3 =
+        [["field_a", "field_b", "field_c"],
+         ["yellow", "黄", "12"],
+         ["tan", "タン", "8.5"],
+         ["brown", "褐色", "29.2"],
+         ["purple", "紫の", "42"],
+         ["red", "赤", "23.8"],
+         ["blue", "青", "12"],
+         ["gray", "グレー", "6.2"],
+         ["white", "白", "1.65"],
+         ["black", "黒", "0.983"],
+         ["pink", "ピンク", "1.1"],
+         ["orange", "オレンジ", "2.5"],
+         ["green", "緑", "0.0072"]];
+
+    /* 1x10 - Simple 1-column file. */
+    string[][] data1x10 =
+        [["field_a"], ["1"], ["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"], ["9"], ["10"]];
+    string fpath_data1x10 = buildPath(testDir, "data1x10.tsv");
+    string fpath_data1x10_noheader = buildPath(testDir, "data1x10_noheader.tsv");
+    writeUnittestTsvFile(fpath_data1x10, data1x10);
+    writeUnittestTsvFile(fpath_data1x10_noheader, data1x10[1..$]);
+
+    string[][] data1x10ExpectedNoWt =
+        [["field_a"], ["8"], ["4"], ["6"], ["5"], ["3"], ["10"], ["7"], ["9"], ["2"], ["1"]];
+
+    string[][] data1x10ExpectedWt1 =
+        [["field_a"], ["8"], ["4"], ["6"], ["10"], ["5"], ["7"], ["9"], ["3"], ["2"], ["1"]];
+
+    /* 2x10a - Uniform distribution [0,1]. */
+    string[][] data2x10a =
+        [["line", "weight"],
+         ["1", "0.26788837"],
+         ["2", "0.06601298"],
+         ["3", "0.38627527"],
+         ["4", "0.47379424"],
+         ["5", "0.02966641"],
+         ["6", "0.05636231"],
+         ["7", "0.70529242"],
+         ["8", "0.91836862"],
+         ["9", "0.99103720"],
+         ["10", "0.31401740"]];
+
+    string fpath_data2x10a = buildPath(testDir, "data2x10a.tsv");
+    writeUnittestTsvFile(fpath_data2x10a, data2x10a);
+
+    string[][] data2x10aExpectedWt2Probs =
+        [["random_weight", "line", "weight"],
+         ["0.968338654945437", "8", "0.91836862"],
+         ["0.918568420544139", "4", "0.47379424"],
+         ["0.257308320877951", "7", "0.70529242"],
+         ["0.237253179070181", "9", "0.99103720"],
+         ["0.160160967018722", "3", "0.38627527"],
+         ["0.0908196626672434", "10", "0.31401740"],
+         ["0.00717645392443612", "6", "0.05636231"],
+         ["4.83186429516301e-08", "1", "0.26788837"],
+         ["3.75256929665355e-10", "5", "0.02966641"],
+         ["8.21232478800958e-13", "2", "0.06601298"]];
+
+    /* 2x10b - Uniform distribution [0,1000]. */
+    string[][] data2x10b =
+        [["line", "weight"],
+         ["1", "761"],
+         ["2", "432"],
+         ["3", "103"],
+         ["4", "448"],
+         ["5", "750"],
+         ["6", "711"],
+         ["7", "867"],
+         ["8", "841"],
+         ["9", "963"],
+         ["10", "784"]];
+
+    string fpath_data2x10b = buildPath(testDir, "data2x10b.tsv");
+    writeUnittestTsvFile(fpath_data2x10b, data2x10b);
+
+    string[][] data2x10bExpectedWt2Probs =
+        [["random_weight", "line", "weight"],
+         ["0.99996486739068", "8", "841"],
+         ["0.999910174671372", "4", "448"],
+         ["0.999608715248737", "6", "711"],
+         ["0.999141885371438", "5", "750"],
+         ["0.999039632502748", "10", "784"],
+         ["0.998896318259319", "7", "867"],
+         ["0.998520583151911", "9", "963"],
+         ["0.995756696791589", "2", "432"],
+         ["0.994087587320506", "1", "761"],
+         ["0.993154677612124", "3", "103"]];
+
+    /* 2x10c - Logarithmic distribution in random order. */
+    string[][] data2x10c =
+        [["line", "weight"],
+         ["1", "31.85"],
+         ["2", "17403.31"],
+         ["3", "653.84"],
+         ["4", "8.23"],
+         ["5", "2671.04"],
+         ["6", "26226.08"],
+         ["7", "1.79"],
+         ["8", "354.56"],
+         ["9", "35213.81"],
+         ["10", "679.29"]];
+
+    string fpath_data2x10c = buildPath(testDir, "data2x10c.tsv");
+    writeUnittestTsvFile(fpath_data2x10c, data2x10c);
+
+    string[][] data2x10cExpectedWt2Probs =
+        [["random_weight", "line", "weight"],
+         ["0.999989390087097", "6", "26226.08"],
+         ["0.999959512916955", "9", "35213.81"],
+         ["0.999916669076135", "8", "354.56"],
+         ["0.999894450521864", "2", "17403.31"],
+         ["0.999758976028616", "5", "2671.04"],
+         ["0.998918527698776", "3", "653.84"],
+         ["0.998891677527825", "10", "679.29"],
+         ["0.995122075068501", "4", "8.23"],
+         ["0.86789371584259", "1", "31.85"],
+         ["0.585744381629156", "7", "1.79"]];
+
+    /* 2x10d. Logarithmic distribution in ascending order. */
+    string[][] data2x10d =
+        [["line", "weight"],
+         ["1", "1.79"],
+         ["2", "8.23"],
+         ["3", "31.85"],
+         ["4", "354.56"],
+         ["5", "653.84"],
+         ["6", "679.29"],
+         ["7", "2671.04"],
+         ["8", "17403.31"],
+         ["9", "26226.08"],
+         ["10", "35213.81"]];
+
+    string fpath_data2x10d = buildPath(testDir, "data2x10d.tsv");
+    writeUnittestTsvFile(fpath_data2x10d, data2x10d);
+
+    string[][] data2x10dExpectedWt2Probs =
+        [["random_weight", "line", "weight"],
+         ["0.999998302218464", "8", "17403.31"],
+         ["0.999978608340414", "10", "35213.81"],
+         ["0.999945638289867", "9", "26226.08"],
+         ["0.999886503635757", "4", "354.56"],
+         ["0.999641619391901", "7", "2671.04"],
+         ["0.999590453389486", "6", "679.29"],
+         ["0.999015744906398", "5", "653.84"],
+         ["0.978031633047474", "3", "31.85"],
+         ["0.799947918069109", "2", "8.23"],
+         ["0.0803742612399491", "1", "1.79"]];
+
+    /* 2x10e. Logarithmic distribution in descending order. */
+    string[][] data2x10e =
+        [["line", "weight"],
+         ["1", "35213.81"],
+         ["2", "26226.08"],
+         ["3", "17403.31"],
+         ["4", "2671.04"],
+         ["5", "679.29"],
+         ["6", "653.84"],
+         ["7", "354.56"],
+         ["8", "31.85"],
+         ["9", "8.23"],
+         ["10", "1.79"]];
+    string fpath_data2x10e = buildPath(testDir, "data2x10e.tsv");
+    writeUnittestTsvFile(fpath_data2x10e, data2x10e);
+
+    string[][] data2x10eExpectedWt2Probs =
+        [["random_weight", "line", "weight"],
+         ["0.999984933489752", "4", "2671.04"],
+         ["0.999959348072026", "3", "17403.31"],
+         ["0.999929957397275", "2", "26226.08"],
+         ["0.999871856792456", "1", "35213.81"],
+         ["0.999574515631739", "6", "653.84"],
+         ["0.999072736502096", "8", "31.85"],
+         ["0.999052603129689", "5", "679.29"],
+         ["0.997303336505164", "7", "354.56"],
+         ["0.840939024352278", "9", "8.23"],
+         ["0.6565001592629", "10", "1.79"]];
+
+    /* Basic tests. Headers and static seed. No weights and weighted. */
+    testTsvSample(["test-a1", "--header", "--static-seed", fpath_dataEmpty], dataEmpty);
+    testTsvSample(["test-a2", "--header", "--static-seed", fpath_data3x0], data3x0);
+    testTsvSample(["test-a3", "-H", "-s", fpath_data3x1], data3x1);
+    testTsvSample(["test-a4", "-H", "-s", fpath_data3x2], data3x2ExpectedNoWt);
+    testTsvSample(["test-a5", "-H", "-s", fpath_data3x3], data3x3ExpectedNoWt);
+    testTsvSample(["test-a6", "-H", "-s", fpath_data3x6], data3x6ExpectedNoWt);
+    testTsvSample(["test-a7", "-H", "-s", "--print-random", fpath_data3x6], data3x6ExpectedNoWtProbs);
+    testTsvSample(["test-a8", "-H", "-s", "--field", "3", fpath_data3x6], data3x6ExpectedWt3);
+    testTsvSample(["test-a9", "-H", "-s", "-p", "-f", "3", fpath_data3x6], data3x6ExpectedWt3Probs);
+
+    /* Basic tests, without headers. */
+    testTsvSample(["test-b1", "-s", fpath_data3x1_noheader], data3x1[1..$]);
+    testTsvSample(["test-b2", "-s", fpath_data3x2_noheader], data3x2ExpectedNoWt[1..$]);
+    testTsvSample(["test-b3", "-s", fpath_data3x3_noheader], data3x3ExpectedNoWt[1..$]);
+    testTsvSample(["test-b4", "-s", fpath_data3x6_noheader], data3x6ExpectedNoWt[1..$]);
+    testTsvSample(["test-b5", "-s", "--print-random", fpath_data3x6_noheader], data3x6ExpectedNoWtProbs[1..$]);
+    testTsvSample(["test-b6", "-s", "--field", "3", fpath_data3x6_noheader], data3x6ExpectedWt3[1..$]);
+    testTsvSample(["test-b7", "-s", "-p", "-f", "3", fpath_data3x6_noheader], data3x6ExpectedWt3Probs[1..$]);
+
+    /* Multi-file tests. */
+    testTsvSample(["test-c1", "--header", "--static-seed",
+                   fpath_data3x0, fpath_data3x3, fpath_data3x1, fpath_dataEmpty, fpath_data3x6, fpath_data3x2],
+                  combo1ExpectedNoWt);
+    testTsvSample(["test-c2", "--header", "--static-seed", "--print-random",
+                   fpath_data3x0, fpath_data3x3, fpath_data3x1, fpath_dataEmpty, fpath_data3x6, fpath_data3x2],
+                  combo1ExpectedNoWtProbs);
+    testTsvSample(["test-c3", "--header", "--static-seed", "--print-random", "--field", "3",
+                   fpath_data3x0, fpath_data3x3, fpath_data3x1, fpath_dataEmpty, fpath_data3x6, fpath_data3x2],
+                  combo1ExpectedWt3Probs);
+    testTsvSample(["test-c4", "--header", "--static-seed", "--field", "3",
+                   fpath_data3x0, fpath_data3x3, fpath_data3x1, fpath_dataEmpty, fpath_data3x6, fpath_data3x2],
+                  combo1ExpectedWt3);
+
+    /* Multi-file, no headers. */
+    testTsvSample(["test-c5", "--static-seed",
+                   fpath_data3x3_noheader, fpath_data3x1_noheader, fpath_dataEmpty,
+                   fpath_data3x6_noheader, fpath_data3x2_noheader],
+                  combo1ExpectedNoWt[1..$]);
+    testTsvSample(["test-c6", "--static-seed", "--print-random",
+                   fpath_data3x3_noheader, fpath_data3x1_noheader, fpath_dataEmpty,
+                   fpath_data3x6_noheader, fpath_data3x2_noheader],
+                  combo1ExpectedNoWtProbs[1..$]);
+    testTsvSample(["test-c7", "--static-seed", "--print-random", "--field", "3",
+                   fpath_data3x3_noheader, fpath_data3x1_noheader, fpath_dataEmpty,
+                   fpath_data3x6_noheader, fpath_data3x2_noheader],
+                  combo1ExpectedWt3Probs[1..$]);
+    testTsvSample(["test-c8", "--static-seed", "--field", "3",
+                   fpath_data3x3_noheader, fpath_data3x1_noheader, fpath_dataEmpty,
+                   fpath_data3x6_noheader, fpath_data3x2_noheader],
+                  combo1ExpectedWt3[1..$]);
+
+    /* Single column file. */
+    testTsvSample(["test-d1", "-H", "-s", fpath_data1x10], data1x10ExpectedNoWt);
+    testTsvSample(["test-d1", "-H", "-s", fpath_data1x10], data1x10ExpectedNoWt);
+
+    /* Distributions. */
+    testTsvSample(["test-d1", "-H", "-s", "-f", "2", "-p", fpath_data2x10a], data2x10aExpectedWt2Probs);
+    testTsvSample(["test-d1", "-H", "-s", "-f", "2", "-p", fpath_data2x10b], data2x10bExpectedWt2Probs);
+    testTsvSample(["test-d1", "-H", "-s", "-f", "2", "-p", fpath_data2x10c], data2x10cExpectedWt2Probs);
+    testTsvSample(["test-d1", "-H", "-s", "-f", "2", "-p", fpath_data2x10d], data2x10dExpectedWt2Probs);
+    testTsvSample(["test-d1", "-H", "-s", "-f", "2", "-p", fpath_data2x10e], data2x10eExpectedWt2Probs);
+}
