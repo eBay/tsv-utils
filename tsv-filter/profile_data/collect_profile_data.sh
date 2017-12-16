52260173a0d337db5adb66b5dea6ba0d81e3ba0d
Jon Degenhardt <jondegenhardt@users.noreply.github.com>
2017-12-16 10:49:04 -0800

    tsv-filter: PGO support

---
+++ tsv-filter/profile_data/collect_profile_data.sh
@@ -0,0 +1,66 @@
+#! /bin/sh
+
+if [ $# -eq 0 ]; then
+    echo "Insufficient arguments. The path of the instrumented program is required."
+    exit 1
+fi
+
+prog=$1
+shift
+
+for f in profile.*.raw; do
+    if [ -e $f ]; then
+        rm $f
+    fi
+done
+
+if [ -e app.profdata ]; then
+   rm -f app.profdata
+fi
+
+$prog profile_data_1.tsv -H --gt 6:-55 --lt 9:-0.2 > /dev/null
+$prog profile_data_1.tsv -H --ge 3:0.002 --le 14:0.0005 > /dev/null
+$prog profile_data_1.tsv -H --invert --le 1:2.2 --lt 16:1.11 > /dev/null
+$prog profile_data_1.tsv -H --or --ge 4:1.65 --lt 5:22.0 > /dev/null
+$prog profile_data_1.tsv -H --invert --or --ge 18:-15 --lt 20:-15 > /dev/null
+$prog profile_data_2.tsv --or --eq 2:646 --eq 2:647 --eq 2:609 > /dev/null
+$prog profile_data_2.tsv --ne 2:622 --ne 2:642 --ne 2:649 > /dev/null
+$prog profile_data_2.tsv --or --eq 3:16 --gt 4:570 --ge 3:140 --le 3:3 --lt 2:510 > /dev/null
+$prog profile_data_5.tsv --ff-eq 2:3 > /dev/null
+$prog profile_data_5.tsv --ff-ne 3:4 > /dev/null
+$prog profile_data_5.tsv --ff-le 3:2 > /dev/null
+$prog profile_data_5.tsv --ff-le 4:2 > /dev/null
+$prog profile_data_5.tsv --ff-lt 4:3 > /dev/null
+$prog profile_data_5.tsv --ff-ge 4:3 > /dev/null
+$prog profile_data_5.tsv --ff-gt 2:4 > /dev/null
+$prog profile_data_5.tsv --ff-str-eq 2:3 > /dev/null
+$prog profile_data_5.tsv --ff-istr-eq 2:3 > /dev/null
+$prog profile_data_5.tsv --ff-str-ne 3:4 > /dev/null
+$prog profile_data_5.tsv --ff-istr-ne 3:4 > /dev/null
+$prog profile_data_1.tsv -H --ff-absdiff-le 10:17:1.11 > /dev/null
+$prog profile_data_1.tsv -H --ff-absdiff-gt 9:10:2.5 > /dev/null
+$prog profile_data_1.tsv -H --ff-reldiff-le 1:17:2.0 > /dev/null
+$prog profile_data_1.tsv -H --ff-reldiff-gt 1:17:2.0 > /dev/null
+$prog profile_data_5.tsv -H --str-eq 1:weiß --str-ne 3:2 > /dev/null
+$prog profile_data_5.tsv -H --or --istr-eq 1:Grün --str-eq 1:日本語 --istr-ne 1:YELLOW > /dev/null
+$prog profile_data_4.tsv -H --str-le 4:cab --str-gt 5:RR > /dev/null
+$prog profile_data_4.tsv -H --invert --str-lt 4:cab --str-ge 5:RR > /dev/null
+$prog profile_data_4.tsv -H --str-in-fld 4:ba --str-not-in-fld 5:T > /dev/null
+$prog profile_data_4.tsv -H --istr-in-fld 4:ab --istr-not-in-fld 5:xx > /dev/null
+$prog profile_data_4.tsv -H --regex 4:'ab[ac]d' > /dev/null
+$prog profile_data_4.tsv -H --regex 4:'b.+c*a' > /dev/null
+$prog profile_data_4.tsv -H --regex 4:'d.+d.+e' > /dev/null
+$prog profile_data_4.tsv -H --iregex 5:'^x.+z$' > /dev/null
+$prog profile_data_4.tsv -H --not-regex 4:'ab[^ab]+ab' --not-iregex 5:xx.+z > /dev/null
+$prog profile_data_4.tsv -H --regex 4:'^e+[^bde]+[bd]' > /dev/null
+$prog profile_data_4.tsv -H --or --blank 3 --is-numeric 3 > /dev/null
+$prog profile_data_4.tsv -H --empty 3 > /dev/null
+$prog profile_data_4.tsv -H --not-empty 3 > /dev/null
+$prog profile_data_4.tsv -H --not-blank 3 > /dev/null
+$prog profile_data_4.tsv -H --is-infinity 2 > /dev/null
+$prog profile_data_4.tsv -H --is-finite 2 > /dev/null
+$prog profile_data_4.tsv -H --is-numeric 3 > /dev/null
+$prog profile_data_4.tsv -H --is-nan 3 > /dev/null
+$prog profile_data_4.tsv -H --is-finite 3 --le 3:30 > /dev/null
+
+ldc-profdata merge -o app.profdata profile.*.raw
