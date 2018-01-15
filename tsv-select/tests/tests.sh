97bb1b18d3006968a2c71671ff7f329eb25385e7
Jon Degenhardt <jondegenhardt@users.noreply.github.com>
2018-01-15 03:34:19 -0800

    tsv-select: Update tests to catch buffer flush case.

--- tsv-select/tests/tests.sh
+++ tsv-select/tests/tests.sh
@@ -145,6 +145,9 @@ ${prog} -V 2>&1 | grep -c 'tsv-select (eBay/tsv-utils-dlang)' >> ${basic_tests_1
 echo "" >> ${basic_tests_1}; echo "====Longer output to trigger buffered output flush===" >> ${basic_tests_1}
 runtest ${prog} "-f 1,3,5,7,8 input_8x50.tsv" ${basic_tests_1}
 
+echo "" >> ${basic_tests_1}; echo "====[tsv-select -H -f 7-8,1,3,5 input_8x50.tsv input_8x50.tsv | wc -l | tr -d ' ']====" >> ${basic_tests_1}
+${prog} -H -f 7-8,1,3,5 input_8x50.tsv input_8x50.tsv | wc -l | tr -d ' ' >>  ${basic_tests_1} 2>&1
+
 ## Error cases
 
 error_tests_1=${odir}/error_tests_1.txt
