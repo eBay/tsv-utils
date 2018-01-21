ce2731d98307e9d4afeac7e13513a7d920020192
Jon Degenhardt <jondegenhardt@users.noreply.github.com>
2018-01-21 03:58:15 -0800

    buffered-output: Do not use lockingTextWriter in unit test (fails with ldc)

--- common/src/tsvutil.d
+++ common/src/tsvutil.d
@@ -489,9 +489,10 @@ unittest
     }
     assert(filepath2.readText == "file2: abcdefghijkl100\n0 1 2 3 4 5 6 7 8 9\n");
 
-    /* With a locking text writer. */
-
-    version(none)
+    /* With a locking text writer. Requires version 2.078.0
+       See: https://issues.dlang.org/show_bug.cgi?id=9661
+     */
+    static if (__VERSION__ >= 2078)
     {
         string filepath3 = buildPath(testDir, "file3.txt");
         {
@@ -503,9 +504,9 @@ unittest
                 ostream.append("file3: ");
                 ostream.append("abc");
                 ostream.append(["def", "ghi", "jkl"]);
-            ostream.appendln("100");
-            ostream.append(iota(0, 10).map!(x => x.to!string).joiner(" "));
-            ostream.appendln();
+                ostream.appendln("100");
+                ostream.append(iota(0, 10).map!(x => x.to!string).joiner(" "));
+                ostream.appendln();
             }
         }
         assert(filepath3.readText == "file3: abcdefghijkl100\n0 1 2 3 4 5 6 7 8 9\n");
