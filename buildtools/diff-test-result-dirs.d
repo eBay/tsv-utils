d9b1818f3302a6327843bad231474da3ead9e809
Jon Degenhardt <jondegenhardt@users.noreply.github.com>
2019-01-13 23:26:16 -0800

    Update copyright dates. (#190)

--- buildtools/diff-test-result-dirs.d
+++ buildtools/diff-test-result-dirs.d
@@ -10,7 +10,7 @@ This tool runs a version of directory diff support multiple correct output versi
 This is to handle the case where different compiler/library versions have different
 valid outputs. The most common case is changes to error message text.
 
-Copyright (c) 2018, eBay Software Foundation
+Copyright (c) 2018-2019, eBay Software Foundation
 Initially written by Jon Degenhardt
 
 License: Boost License 1.0 (http://boost.org/LICENSE_1_0.txt)
