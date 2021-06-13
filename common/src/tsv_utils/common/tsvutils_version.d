7c6a3ddbc0801e073f62053c3f9967afc863f57c
Jon Degenhardt <jondegenhardt@users.noreply.github.com>
2021-06-12 17:33:01 -0700

    Bump version and update compiler version for pre-built binaries.

--- common/src/tsv_utils/common/tsvutils_version.d
+++ common/src/tsv_utils/common/tsvutils_version.d
@@ -3,7 +3,7 @@
 
 module tsv_utils.common.tsvutils_version;
 
-enum string tsvutilsVersion = "v2.2.0";
+enum string tsvutilsVersion = "v2.2.1";
 
 string tsvutilsVersionNotice (string toolName) @safe pure nothrow
 {
